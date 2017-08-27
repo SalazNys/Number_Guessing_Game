7f3db4ab217845448b1078ddc23c5bb05b1e820d
Salazar <salaznys@gmail.com>
2017-08-27 19:25:42 -0400

    Add files via upload

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,23 +1,46 @@
-
-puts "|---------------------|"
-puts "|Number Guessing Game:|"
-puts "|---------------------|"
-puts " Please guess a number bewteen 1 and 100"
-number = rand(1..100)
-guess = gets.chomp. to_i
-trial = 1
-until guess == number || trial == 5 do
-  if guess < number
-    puts "you are low, try again"
-  elsif guess > number
-    puts "you are high, try again"
-  end
-  trial+= 1
-  guess = gets.chomp.to_i
+def random_number()
+  number = rand(1..100)
 end
-if (guess == number)
-puts "You guessed #{number} in #{trial}
-     attempt#{'s' if trial > 1}!"
-else
-  puts " sorry you lost after #{trial} unsuccessful attempts"
+def emptynil(guess)
+  return true if guess.empty? || guess.nil?
+  return false
+end
+
+previous_guesses = []
+correct_number = random_number
+counter = 0
+puts"---------------------------------------------------------------|"
+puts "------------------NUMBER GUESSING GAME------------------------|"
+puts"---------------------------------------------------------------|"
+
+puts "You have up to 5 chances to guess the right number!"
+puts "What is your guess?"
+
+loop do
+  guess = gets.chomp
+  if emptynil(guess)
+    puts "Nothing has been entered as a number, please guess agin!"
+  next
+end
+
+  if previous_guesses.include?(guess.to_i)
+    puts "you just guessed the same number,Guess again!"
+
+  elsif guess.to_i > correct_number.to_i
+    puts "you guessed too high! Guess again!"
+    previous_guesses << guess.to_i
+
+  elsif guess.to_i < correct_number.to_i
+    puts "you guessed too low! Guess again!"
+    previous_guesses << guess.to_i
+
+  elsif correct_number.to_i == guess.to_i
+    puts "CONGRATULATION YOU WON!"
+    break
+  end
+     counter += 1
+  if counter == 5
+    puts "SORRY YOU LOST!"
+    break
+  end
 end
